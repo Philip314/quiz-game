16a5416199035bd27b9f24053821185e171d9e36
Philip314 <philip956@hotmail.co.uk>
2022-05-02 22:49:34 +0100

    Create slice of problems from file

--- main.go
+++ main.go
@@ -17,16 +17,32 @@ func main() {
 
 	reader := csv.NewReader(file)
 
-	lines, err := reader.ReadAll()
+	records, err := reader.ReadAll()
 	if err != nil {
 		os.Exit(1)
 	}
 
-	fmt.Println(lines)
+	fmt.Println(createProblems(records))
+}
+
+type Problem struct {
+	Question string
+	Answer   string
 }
 
 func getFilename() string {
 	csvFilename := flag.String("csv", "problems.csv", "CSV file containing the questions and answers")
 	flag.Parse()
 	return *csvFilename
+}
+
+func createProblems(records [][]string) []Problem {
+	problems := make([]Problem, len(records))
+	for i, row := range records {
+		problems[i] = Problem{
+			Question: row[0],
+			Answer:   row[1],
+		}
+	}
+	return problems
 }
